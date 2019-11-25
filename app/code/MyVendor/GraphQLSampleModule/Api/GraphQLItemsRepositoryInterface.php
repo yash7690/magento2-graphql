cf1815e47266887f44aeef4ec76d8badeee7b943
Yash Shah <yash7june@gmail.com>
2019-11-25 22:34:39 +0530

    GraphQL Sample Module

--- app/code/MyVendor/GraphQLSampleModule/Api/GraphQLItemsRepositoryInterface.php
+++ app/code/MyVendor/GraphQLSampleModule/Api/GraphQLItemsRepositoryInterface.php
@@ -15,7 +15,7 @@ interface GraphQLItemsRepositoryInterface
      * Retrieve GraphQLItems list
      *
      * @param \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria
-     * @return \MyVendor\MyVendor\Api\Data\GraphQLItemsSearchResultInterface
+     * @return \MyVendor\GraphQLSampleModule\Api\Data\GraphQLItemsSearchResultsInterface
      * @throws \Magento\Framework\Exception\LocalizedException
      */
     public function getList(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria);
---
+++ app/code/MyVendor/MyModule/registration.php
@@ -0,0 +1,7 @@
+<?php
+ 
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'MyVendor_MyModule',
+    __DIR__
+);
